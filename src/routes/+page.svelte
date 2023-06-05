456c76e8ede8f93bb03eb51a973603078fd2d6a7
Fabrício Duarte <fabricioohod@outlook.com>
2023-06-05 07:39:44 -0300

    Update version number

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -160,11 +160,11 @@
         >
             <div class="pt-4">
                 <div class="grid grid-cols-5">
-                    <span class=" -translate-x-5 max-2xl:-translate-x-3 max-2xl:text-sm">Upgrade</span>
-                    <span class=" -translate-x-5 max-2xl:-translate-x-4 max-2xl:text-sm">Treino</span>
-                    <span class=" translate-x-5 max-2xl:-translate-x-1 max-2xl:text-sm">Dado</span>
-                    <span class=" translate-x-5 max-2xl:-translate-x-3 max-2xl:text-sm">Perícia</span>
-                    <span class=" translate-x-[4.5rem] max-2xl:translate-x-12 max-2xl:text-sm">Bonus</span>
+                    <span class=" -translate-x-6 max-2xl:-translate-x-3 max-2xl:text-sm">Upgrade</span>
+                    <span class=" -translate-x-6 max-2xl:-translate-x-4 max-2xl:text-sm">Treino</span>
+                    <span class=" translate-x-0 max-2xl:-translate-x-1 max-2xl:text-sm">Dado</span>
+                    <span class=" translate-x-0 max-2xl:-translate-x-3 max-2xl:text-sm">Perícia</span>
+                    <span class=" translate-x-12 max-2xl:translate-x-12 max-2xl:text-sm">Bonus</span>
                 </div>
                 {#each $MainProficiencys as proficiency, i}
                     <Prominence data={proficiency} />
