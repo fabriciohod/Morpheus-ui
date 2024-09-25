f1ebdc745ad570ea07ec5848b16c93fdc16dfbb0
Fabricio.Duarte <_>
2024-09-25 19:14:40 -0300

    feat: Adding proficiency`s section

--- src/routes/character/[id]/+page.svelte
+++ src/routes/character/[id]/+page.svelte
@@ -47,100 +47,135 @@
 		</section>
 	</label>
 </header>
-<div class="w-56 bg-zinc-900/35 p-4 polygonL">
-	<label for="roll-dice" class="flex">
-		<button class="w-14 h-10 bg-zinc-700/45 polygonR mr-1 active:bg-zinc-500 transition-all">
-			<Icon icon="fa-solid:dice-d20" class="w-5 h-5 m-auto" />
-		</button>
-		<input type="text" id="roll-dice" class="w-full bg-zinc-700/45 polygonL text-center placeholder:text-white/15" placeholder="1d20 + 69" />
-	</label>
-</div>
-<div class="flex w-56 bg-zinc-900/35 p-4 polygonL mt-2">
-	<div class="w-full translate-x-6">
-		<label for="con" class="grid grid-cols-2 mb-2">
-			<span class="mr-2">Constituição</span>
-			<input
-				type="number"
-				id="con"
-				class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-				placeholder="0"
-				min="0"
-				max="20"
-			/>
-		</label>
-		<label for="con" class="grid grid-cols-2 mb-2">
-			<span class="mr-2">Constituição</span>
-			<input
-				type="number"
-				id="con"
-				class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-				placeholder="0"
-				min="0"
-				max="20"
-			/>
-		</label>
-		<label for="dex" class="grid grid-cols-2 mb-2">
-			<span class="mr-2">Destreza</span>
-			<input
-				type="number"
-				id="dex"
-				class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-				placeholder="0"
-				min="0"
-				max="20"
-			/>
-		</label>
-		<label for="int" class="grid grid-cols-2 mb-2">
-			<span class="mr-2">Intelecto</span>
-			<input
-				type="number"
-				id="int"
-				class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-				placeholder="0"
-				min="0"
-				max="20"
-			/>
-		</label>
-		<label for="str" class="grid grid-cols-2">
-			<span class="mr-2">Força</span>
-			<input
-				type="number"
-				id="str"
-				class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-				placeholder="0"
-				min="0"
-				max="20"
-			/>
-		</label>
-	</div>
-</div>
-<div class=" bg-zinc-900/35 w-56 p-4 mt-2 relative polygonL">
-	<div class="">
-		<div id="health-bar-hidden" class="polygonR">
-			<div
-				class="flex justify-center items-center w-full h-10 bg-red-800 z-[1]"
-				style="width: 100%;"
-			></div>
+<div class="flex flex-row w-full h-screen">
+	<div class="flex flex-col w-fit h-fit bg-zinc-900/35 polygonR p-4">
+		<div class="w-56 bg-zinc-900/35 p-4 polygonL">
+			<label for="roll-dice" class="flex">
+				<button class="w-14 h-10 bg-zinc-700/45 polygonR mr-1 active:bg-zinc-500 transition-all">
+					<Icon icon="fa-solid:dice-d20" class="w-5 h-5 m-auto" />
+				</button>
+				<input
+					type="text"
+					id="roll-dice"
+					class="w-full bg-zinc-700/45 polygonL text-center placeholder:text-white/15"
+					placeholder="1d20 + 69"
+				/>
+			</label>
 		</div>
-		<div
-			id="health-bar-front"
-			class="polygonR absolute w-full h-10 -translate-y-10 bg-red-500"
-			style="width: 50%;"
-		></div>
+		<div class="flex w-56 bg-zinc-900/35 p-4 polygonL mt-2">
+			<div class="w-full translate-x-6">
+				<label for="con" class="grid grid-cols-2 mb-2">
+					<span class="mr-2">Constituição</span>
+					<input
+						type="number"
+						id="con"
+						class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+						placeholder="0"
+						min="0"
+						max="20"
+					/>
+				</label>
+				<label for="con" class="grid grid-cols-2 mb-2">
+					<span class="mr-2">Constituição</span>
+					<input
+						type="number"
+						id="con"
+						class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+						placeholder="0"
+						min="0"
+						max="20"
+					/>
+				</label>
+				<label for="dex" class="grid grid-cols-2 mb-2">
+					<span class="mr-2">Destreza</span>
+					<input
+						type="number"
+						id="dex"
+						class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+						placeholder="0"
+						min="0"
+						max="20"
+					/>
+				</label>
+				<label for="int" class="grid grid-cols-2 mb-2">
+					<span class="mr-2">Intelecto</span>
+					<input
+						type="number"
+						id="int"
+						class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+						placeholder="0"
+						min="0"
+						max="20"
+					/>
+				</label>
+				<label for="str" class="grid grid-cols-2">
+					<span class="mr-2">Força</span>
+					<input
+						type="number"
+						id="str"
+						class=" text-center w-12 p-1 text-white bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+						placeholder="0"
+						min="0"
+						max="20"
+					/>
+				</label>
+			</div>
+		</div>
+		<div class=" bg-zinc-900/35 w-56 p-4 mt-2 relative polygonL">
+			<div class="">
+				<div id="health-bar-hidden" class="polygonR">
+					<div
+						class="flex justify-center items-center w-full h-10 bg-red-800 z-[1]"
+						style="width: 100%;"
+					></div>
+				</div>
+				<div
+					id="health-bar-front"
+					class="polygonR absolute w-full h-10 -translate-y-10 bg-red-500"
+					style="width: 50%;"
+				></div>
+			</div>
+			<span class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">100/100</span>
+		</div>
+		<div class="flex justify-around w-56 bg-zinc-900/35 p-4 mt-2 polygonL">
+			<div class="relative w-8" title="Defesa">
+				<Icon icon="solar:shield-minimalistic-line-duotone" height="3.3rem" />
+				<span
+					class="absolute top-1/2 left-1/2 translate-x-[.09rem] -translate-y-3 mix-blend-difference"
+					>99</span
+				>
+			</div>
+			<div class="relative w-8" title="Esquiva">
+				<Icon icon="game-icons:dodge" height="3.3rem" />
+				<span
+					class="absolute top-1/2 left-1/2 -translate-x-[.09rem] translate-y-1 mix-blend-difference"
+					>99</span
+				>
+			</div>
+		</div>
 	</div>
-	<span class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">100/100</span>
-</div>
-<div class="flex justify-around w-56 bg-zinc-900/35 p-4 mt-2 polygonL">
-	<div class="relative w-8" title="Defesa">
-		<Icon icon="solar:shield-minimalistic-line-duotone" height="3.3rem" />
-		<span class="absolute top-1/2 left-1/2 translate-x-[.09rem] -translate-y-3 mix-blend-difference"
-			>99</span
-		>
-	</div>
-	<div class="relative w-8" title="Esquiva">
-		<Icon icon="game-icons:dodge" height="3.3rem" />
-		<span class="absolute top-1/2 left-1/2 -translate-x-[.09rem] translate-y-1 mix-blend-difference"
-			>99</span
-		>
+	<div class="flex flex-col w-fit h-fit bg-zinc-900/35 polygonR p-4 mx-2">
+		{#each [1, 2, 3] as _}
+			<div class="bg-zinc-900/35 polygonL my-1 p-2">
+				<input
+					type="checkbox"
+					class="h-4 w-4 appearance-none bg-zinc-700 checked:bg-yellow-600 hover:bg-yellow-700 clipped-small"
+				/>
+				<select name="profLevel" id="profLevel" class="text-white w-30 mx-2 p-1 bg-zinc-700/45 polygonL">
+					<option value="1">Leigo</option>
+					<option value="2">Trainado</option>
+				</select>
+				<select name="atribute" id="atribute" class="text-white w-30 p-1 bg-zinc-700/45 polygonL">
+					<option value="@for">Força</option>
+					<option value="@dex">Destreza</option>
+				</select>
+				<span class="mx-2">Pilotagem</span>
+				<input
+					type="number"
+					min="0"
+					class="w-12 p-1 text-center bg-zinc-700/45 polygonL [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+				/>
+			</div>
+		{/each}
 	</div>
 </div>
